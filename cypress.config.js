c54747d6cf8412623ff1dd1d57b8336a06f7cb29
franciellyfp <franciellyfpedroso@gmail.com>
2024-01-29 14:48:55 -0300

    fixing eslint

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   chromeWebSecurity: false,
-  projectId: "fiq5ug",
+  projectId: 'fiq5ug',
   e2e: {
     baseUrl: 'https://notes-serverless-app.com',
     defaultCommandTimeout: 30000,
